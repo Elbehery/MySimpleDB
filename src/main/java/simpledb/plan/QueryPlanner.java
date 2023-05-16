66951e737a5f57bce88e9c686394f654a840e4dc
Mustafa Elbehery <melbeher@redhat.com>
2023-05-16 12:50:21 +0200

    reformat project

--- src/main/java/simpledb/plan/QueryPlanner.java
+++ src/main/java/simpledb/plan/QueryPlanner.java
@@ -4,18 +4,19 @@ import simpledb.tx.Transaction;
 import simpledb.parse.QueryData;
 
 /**
- * The interface implemented by planners for 
+ * The interface implemented by planners for
  * the SQL select statement.
- * @author Edward Sciore
  *
+ * @author Edward Sciore
  */
 public interface QueryPlanner {
-   
-   /**
-    * Creates a plan for the parsed query.
-    * @param data the parsed representation of the query
-    * @param tx the calling transaction
-    * @return a plan for that query
-    */
-   public Plan createPlan(QueryData data, Transaction tx);
+
+    /**
+     * Creates a plan for the parsed query.
+     *
+     * @param data the parsed representation of the query
+     * @param tx   the calling transaction
+     * @return a plan for that query
+     */
+    public Plan createPlan(QueryData data, Transaction tx);
 }
