0f540ea7ae3c21bf64d9244c3f06750634cd37e4
Mustafa Elbehery <melbeher@redhat.com>
2023-08-20 13:07:56 +0200

    revert "ex 6.12"
    This reverts commit 5dd3d342f27ac30a8afe8419fa4571976e80a385.

--- src/main/java/simpledb/record/Layout.java
+++ src/main/java/simpledb/record/Layout.java
@@ -86,10 +86,8 @@ public class Layout {
         int fldtype = schema.type(fldname);
         if (fldtype == INTEGER)
             return Integer.BYTES;
-        else { // fldtype == VARCHAR
-            int paddedLength = schema.length(fldname) % 4;
-            return Page.maxLength(schema.length(fldname) + paddedLength);
-        }
+        else // fldtype == VARCHAR
+            return Page.maxLength(schema.length(fldname));
     }
 }
 
