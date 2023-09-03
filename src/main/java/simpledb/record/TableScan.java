effac12ac649add2231686181d1271a4d622f023
Mustafa Elbehery <melbeher@redhat.com>
2023-09-03 12:06:09 +0200

    Revert "ex 8.16"
    This reverts commit d4f68627a36968be094d7cd5db8ead9e55f55140.

--- src/main/java/simpledb/record/TableScan.java
+++ src/main/java/simpledb/record/TableScan.java
@@ -113,10 +113,6 @@ public class TableScan implements UpdateScan {
         return new RID(rp.block().number(), currentslot);
     }
 
-    public Layout getLayout() {
-        return layout;
-    }
-
     // Private auxiliary methods
 
     private void moveToBlock(int blknum) {
