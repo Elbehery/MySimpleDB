0cca972af3eeac8003afc15b29e0027063fcaded
Mustafa Elbehery <melbeher@redhat.com>
2023-11-13 17:33:30 +0100

    ex 9.5

--- src/main/java/simpledb/parse/PredParser.java
+++ src/main/java/simpledb/parse/PredParser.java
@@ -3,36 +3,52 @@ package simpledb.parse;
 public class PredParser {
     private Lexer lex;
 
+    private StringBuilder parseTree;
+
     public PredParser(String s) {
         lex = new Lexer(s);
+        parseTree = new StringBuilder();
     }
 
     public String field() {
+        String id = lex.eatId();
+        String treeNode = "/" +
+                id;
+        parseTree.append(treeNode);
         return lex.eatId();
     }
 
     public void constant() {
-        if (lex.matchStringConstant())
-            lex.eatStringConstant();
-        else
-            lex.eatIntConstant();
+        String treeNode = "/\n";
+        if (lex.matchStringConstant()) {
+            treeNode += lex.eatStringConstant();
+        } else {
+            treeNode += lex.eatIntConstant();
+        }
+        parseTree.append(treeNode);
     }
 
     public void expression() {
-        if (lex.matchId())
-            field();
-        else
+        String treeNode = "/\n";
+        if (lex.matchId()) {
+            treeNode += field();
+            parseTree.append(treeNode);
+        } else
             constant();
     }
 
     public void term() {
+        String treeNode = "exp\n";
         expression();
         lex.eatDelim('=');
         expression();
+        parseTree.append(treeNode);
     }
 
     public void predicate() {
+        String treeNode = "pred\n";
         term();
+        parseTree.append(treeNode);
         if (lex.matchKeyword("and")) {
             lex.eatKeyword("and");
             predicate();
