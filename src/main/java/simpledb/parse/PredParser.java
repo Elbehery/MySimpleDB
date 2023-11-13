3ff18693b54c5f4a1854c09866121b37d718d9c8
Mustafa Elbehery <melbeher@redhat.com>
2023-11-13 17:34:26 +0100

    Revert "ex 9.5"
    This reverts commit 0cca972af3eeac8003afc15b29e0027063fcaded.

--- src/main/java/simpledb/parse/PredParser.java
+++ src/main/java/simpledb/parse/PredParser.java
@@ -3,52 +3,36 @@ package simpledb.parse;
 public class PredParser {
     private Lexer lex;
 
-    private StringBuilder parseTree;
-
     public PredParser(String s) {
         lex = new Lexer(s);
-        parseTree = new StringBuilder();
     }
 
     public String field() {
-        String id = lex.eatId();
-        String treeNode = "/" +
-                id;
-        parseTree.append(treeNode);
         return lex.eatId();
     }
 
     public void constant() {
-        String treeNode = "/\n";
-        if (lex.matchStringConstant()) {
-            treeNode += lex.eatStringConstant();
-        } else {
-            treeNode += lex.eatIntConstant();
-        }
-        parseTree.append(treeNode);
+        if (lex.matchStringConstant())
+            lex.eatStringConstant();
+        else
+            lex.eatIntConstant();
     }
 
     public void expression() {
-        String treeNode = "/\n";
-        if (lex.matchId()) {
-            treeNode += field();
-            parseTree.append(treeNode);
-        } else
+        if (lex.matchId())
+            field();
+        else
             constant();
     }
 
     public void term() {
-        String treeNode = "exp\n";
         expression();
         lex.eatDelim('=');
         expression();
-        parseTree.append(treeNode);
     }
 
     public void predicate() {
-        String treeNode = "pred\n";
         term();
-        parseTree.append(treeNode);
         if (lex.matchKeyword("and")) {
             lex.eatKeyword("and");
             predicate();
